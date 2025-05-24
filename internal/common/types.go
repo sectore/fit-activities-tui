aaa04647c17aa23b819e0e524d609f0edc75e9f0
jk <47693+sectore@users.noreply.github.com>
2025-05-24 12:31:00 +0200

    next/prrev/current

--- internal/common/types.go
+++ internal/common/types.go
@@ -27,6 +27,71 @@ func ActivitySuccess(data ActivityData) ActivityAD {
 }
 
 type Activity struct {
-	Path string
-	Data ActivityAD
+	Path     string
+	Selected bool
+	Data     ActivityAD
+}
+
+type Activities struct {
+	inner []Activity
+}
+
+// Set / get `next` selected `Activity` from `Activities`
+func (m Activities) Next() (*Activity, bool) {
+	// ignore empty lists
+	if len(m.inner) == 0 {
+		return nil, false
+	}
+
+	for i, act := range m.inner {
+		if act.Selected {
+			// unselect current
+			m.inner[i].Selected = false
+			// select next
+			next := (i + 1) % len(m.inner)
+			m.inner[next].Selected = true
+			return &m.inner[next], true
+		}
+	}
+	// In case no `Activity` was selected, select first
+	m.inner[0].Selected = true
+	return &m.inner[0], true
+}
+
+// Set / get `prev` selected `Activity` from `Activities`
+func (m Activities) Prev() (*Activity, bool) {
+	// ignore empty lists
+	if len(m.inner) == 0 {
+		return nil, false
+	}
+
+	for i, act := range m.inner {
+		if act.Selected {
+			// unselect current
+			m.inner[i].Selected = false
+			// select prev
+			prev := (i + len(m.inner) - 1) % len(m.inner)
+			m.inner[prev].Selected = true
+			return &m.inner[prev], true
+		}
+	}
+	// In case no `Activity` was selected, select first
+	m.inner[0].Selected = true
+	return &m.inner[0], true
+}
+
+// Set / get `prev` selected `Activity` from `Activities`
+func (m Activities) Current() (*Activity, bool) {
+	// ignore empty lists
+	if len(m.inner) == 0 {
+		return nil, false
+	}
+
+	for _, act := range m.inner {
+		if act.Selected {
+			return &act, true
+		}
+	}
+
+	return nil, true
 }
