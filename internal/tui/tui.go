08a6256ea7426260b697fac271de5c856aa24d9d
jk <47693+sectore@users.noreply.github.com>
2025-05-27 10:24:07 +0200

    extract listDelegate

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -2,7 +2,6 @@ package tui
 
 import (
 	"fmt"
-	"io"
 	"path/filepath"
 	"time"
 
@@ -24,67 +23,6 @@ type Model struct {
 	list        list.Model
 }
 
-type listDelegate struct {
-	DefaultDelegate list.DefaultDelegate
-	Spinner         spinner.Model
-}
-
-func (d listDelegate) Height() int  { return 2 }
-func (d listDelegate) Spacing() int { return 1 }
-
-func NewListDelegate(spinner *spinner.Model) listDelegate {
-
-	s := list.NewDefaultItemStyles()
-	s.NormalTitle = lipgloss.NewStyle().
-		Padding(0, 0, 0, 2) //nolint:mnd
-	s.DimmedTitle = s.NormalTitle
-	s.NormalDesc = s.NormalTitle
-	s.DimmedDesc = s.NormalDesc
-	selectedStyle := lipgloss.NewStyle().
-		Border(lipgloss.OuterHalfBlockBorder(), false, false, false, true).
-		Padding(0, 0, 0, 1)
-	s.SelectedTitle = selectedStyle.
-		Bold(true)
-	s.SelectedDesc = selectedStyle
-	s.FilterMatch = lipgloss.NewStyle().Bold(true)
-
-	d := list.NewDefaultDelegate()
-
-	d.Styles = s
-
-	cd := listDelegate{DefaultDelegate: d, Spinner: *spinner}
-	return cd
-}
-
-func (d *listDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
-	// `ActivityAD` is our custom `Item`
-	if act, ok := item.(common.Activity); ok {
-		_, _, loading := asyncdata.Loading(act.Data)
-		notAsked := asyncdata.NotAsked(act.Data)
-		if loading || notAsked {
-			d.Spinner.Style = lipgloss.NewStyle().MarginBottom(1).MarginLeft(2)
-			fmt.Fprintf(w, "%s", d.Spinner.View())
-			return
-		}
-	}
-	// TODO: render `Failure`
-
-	// use default render
-	d.DefaultDelegate.Render(w, m, index, item)
-}
-
-// Delegate `Update` to have still an animated spinner for each item
-func (d *listDelegate) Update(msg tea.Msg, _ *list.Model) tea.Cmd {
-	switch msg := msg.(type) {
-	case spinner.TickMsg:
-		var cmd tea.Cmd
-		s, cmd := d.Spinner.Update(msg)
-		d.Spinner = s
-		return cmd
-	}
-	return nil
-}
-
 const (
 	pageActiveBullet   = "●"
 	pageInactiveBullet = "∙"
@@ -94,7 +32,7 @@ func InitialModel(path string) Model {
 
 	s := spinner.New()
 	s.Spinner = spinner.MiniDot
-	// Note: We do need to pass `Spinner` down to the `ItemDelegate` of the list
+	// Note: We do need to pass `Spinner` down to the `ListDelegate` of the list
 	// to make sure `spinner.Tick` is fired once. Currently in `Init`.
 	delegate := NewListDelegate(&s)
 	list := list.New([]list.Item{}, &delegate, 20, 0)
@@ -237,10 +175,6 @@ var (
 	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
 	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
 )
-
-type Content struct {
-	common.Activities
-}
 
 func (m Model) contentView() string {
 	var view string = ""
